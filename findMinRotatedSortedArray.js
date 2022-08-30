53ead38b9ffa964b830a5253f532775e0e59bcce
John Economou <johnlafayeti@gmail.com>
2022-08-30 11:16:02 -0400

    finished findMinRotatoes

--- findMinRotatedSortedArray.js
+++ findMinRotatedSortedArray.js
@@ -1,18 +1,28 @@
-nums = [3,4,5,1,2];
+// nums = [3,4,5,1,2];
+nums = [4,5,0,1,2,3];
 
 var findMin = function(nums) {
-  var midPoint = (Math.floor(nums.length/2));
   var right = nums.length - 1;
   var left = 0;
+  var res = nums[0];
 
   while (left <= right) {
+    // condition where its not rotated at all
     if (nums[left] < nums[right]) {
-      right = midPoint;
-    } else {
-      left = midPoint;
+      res = Math.min(res, nums[left]);
+      break;
     }
     midPoint = (Math.floor((left + right)/2));
+    res = Math.min(res, nums[midPoint]);
+    // part of the sorted array that has been rotated
+    if (nums[midPoint] > nums[left]) {
+      left = midPoint + 1;
+    } else {
+      right = midPoint - 1;
+    }
   }
+  console.log(res);
+  return res;
 };
 
 findMin(nums);
