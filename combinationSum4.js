8a497fe4f69561ec006b534e604a3e1155957c60
John Economou <johnlafayeti@gmail.com>
2022-09-11 17:31:40 -0400

    bit confusing but it works

--- combinationSum4.js
+++ combinationSum4.js
@@ -1,3 +1,14 @@
-var combinationSum4 = function(nums, target) {
-    
+var combinationSum4 = function (nums, target) {
+    var dp = Array(target + 1).fill(0);
+    dp[0] = 1;
+
+    for (var i = 0; i <= target; i++) {
+        for (var j = 0; j < nums.length; j++) {
+            if (i - nums[j] >= 0) { dp[i] =  dp[i] + dp[i - nums[j]] }
+        }
+    }
+    console.log(dp[target]);
+    return dp[target];
 };
+
+combinationSum4([1, 2, 3], 4);
